2cff1653f630f4d254509a4b93800780a68e930d
jinnic <yoohyojin@gmail.com>
2020-07-14 18:11:16 -0400

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react'
+
+const Keypad = () => {
+  
+  const hadleKeyUP = (e) => {
+    // e.key
+    console.log('Entering password...')
+  }
+
+  return (
+    <input type="password" onKeyUp={hadleKeyUP}/>
+  )
+}
+
+export default Keypad
